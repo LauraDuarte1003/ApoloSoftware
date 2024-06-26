914512aa6b5ec2ff765ed5cd244ebd932b5492a1
Jhan Arevalo <carlos2525x@gmail.com>
2024-06-26 16:44:59 -0500

    modificacion de link

--- Index.js
+++ Index.js
@@ -3,12 +3,15 @@ const express = require ("express");
 //odjetos para llamar los metodos de express
 const app =express();
 
-//rut  inicial
+/*rut  inicial
 app.get("/",function(req,res){
     res.send("<h1> Hola Aprendises")
-});
+});*/
+
+//ruta de archivos estaticos
+app.use(express.static("public"));
 
 //configurar el puerto usado oara ek servidor local
 app.listen(5000,function(){
-    console.log("El servidor es  http://localhost:5000")
+    console.log("El servidor es  http://localhost:5000");
 });
