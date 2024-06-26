e74f913ae40d433e8f8877003f9195b8641ef112
Jhan Arevalo <carlos2525x@gmail.com>
2024-06-26 10:17:01 -0500

    se integra exprexx

---
+++ Index.js
@@ -0,0 +1,14 @@
+//Importar la libreria
+const express = require ("express");
+//odjetos para llamar los metodos de express
+const app =express();
+
+//rut  inicial
+app.get("/",function(req,res){
+    res.send("<h1> Hola Aprendises")
+});
+
+//configurar el puerto usado oara ek servidor local
+app.listen(5000,function(){
+    console.log("El servidor es  http://localhost:5000")
+});
